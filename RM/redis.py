90995780d8d171ef0b251666e90c91d2c0721b9f
ale10bb <ale10bb@git.com>
2023-03-15 14:07:41 +0800

    refactor(redis): add typing

--- RM/redis.py
+++ RM/redis.py
@@ -4,12 +4,13 @@ import json
 import redis
 import socket
 
+
 class RedisStream:
     ''' Redis Stream 的封装客户端，实现消息队列管理
     '''
-    _r = None
+    _r: redis.Redis = None
 
-    def __init__(self, host:str, password:str=''):
+    def __init__(self, host: str, password: str = ''):
         ''' 初始化 Redis Stream 的配置
 
         Args:
@@ -17,16 +18,17 @@ class RedisStream:
         '''
         logger = logging.getLogger(__name__)
         self._r = redis.Redis(
-            host=host, 
-            port=6379, 
-            db=0, 
+            host=host,
+            port=6379,
+            db=0,
             password=password,
             decode_responses=True,
         )
-        assert self._r.ping()
-        logger.info('Redis configration ({}) confirmed.'.format(host))
-    
-    def add(self, command:str, kwargs:dict={}):
+        if not self._r.ping():
+            raise ValueError('Cannot init redis.')
+        logger.info('Redis configration (%s) confirmed.', host)
+
+    def add(self, command: str, kwargs: dict):
         ''' 在Stream中插入一条指令
 
         Args:
@@ -34,31 +36,31 @@ class RedisStream:
             kwargs(dict): 操作参数
         '''
         logger = logging.getLogger(__name__)
-        logger.debug('args: {}'.format({'command': command, 'kwargs': kwargs}))
+        logger.debug('args: %s', {'command': command, 'kwargs': kwargs})
+        if not isinstance(kwargs, dict):
+            kwargs = {}
         self._r.xadd('RM', {'command': command, 'kwargs': json.dumps(kwargs)})
-        logger.debug('len: {}'.format(self._r.xlen('RM')))
+        logger.debug('len: %s', self._r.xlen('RM'))
 
     def read(self) -> dict:
         ''' 在Stream以阻塞方式读取一条指令
-
         '''
         logger = logging.getLogger(__name__)
         l = self._r.xreadgroup(
             groupname='RMConsumers',
-            consumername=socket.gethostname(), 
-            streams={'RM':'>'}, 
-            count=1, 
+            consumername=socket.gethostname(),
+            streams={'RM': '>'},
+            count=1,
             block=0,
             noack=True,
         )
-        logger.debug('l: {}'.format(l))
+        logger.debug('len: %s', l)
         value = l[0][1][0][1]
         value['kwargs'] = json.loads(value['kwargs'])
         return value
 
     def trim(self):
         ''' 修剪Stream长度至10
-
         '''
         logger = logging.getLogger(__name__)
-        logger.debug('original len: {}'.format(self._r.xtrim(name='RM', maxlen=10)))
+        logger.debug('original len: %s', self._r.xtrim(name='RM', maxlen=10))
